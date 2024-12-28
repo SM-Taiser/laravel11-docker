2de12c0f9e671bc74c838f9b002c854d2c49d6dc
SM Taiser <smtaiser123@gmail.com>
2024-12-29 00:00:15 +0600

    updated

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,15 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
-
-Route::get('/test', function () {
+Route::get('/', function () {
     return Inertia::render('Welcome', [
-        'data' => [1, 2]
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
 });
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
